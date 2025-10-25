b487d29aef6facff076b3de9342522555531aa0e
Liliana (MKia) <mkia-contacto@gmail.com>
2025-10-25 15:32:05 -0600

    Actualiza mensaje de Bot-ito

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 def read_root():
     return {"message": "Hola Liliana 🌞, tu API Bot-ito está viva!"}
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hola Liliana 🌞, Bot-ito API ahora está actualizada correctamente."}
